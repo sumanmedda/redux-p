45ca559fb9019c9ecac46806a2acf173a7ccee3e
sumanmedda <sumanmedda33@gmail.com>
2024-05-14 11:45:43 +0530

    added route

---
+++ src/routes/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Outlet } from 'react-router-dom'
+
+function App() {
+  
+  return (
+    <>
+      <h1>App</h1>
+      <Outlet />
+    </>
+  )
+}
+
+export default App
