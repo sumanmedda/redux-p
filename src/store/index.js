8b6630ac8058e18322ae04018d5bdbcf4c225f0c
sumanmedda <sumanmedda33@gmail.com>
2024-05-14 13:09:17 +0530

    added input and buttons

--- src/store/index.js
+++ src/store/index.js
@@ -4,15 +4,19 @@ const INITIAL_VALUE = {
   counter: 0
 }
 
-const reducer = (store = INITIAL_VALUE, action) => {
+const reducer = (store, action) => {
   if(action.type === 'INCREMENT') {
     store = {counter : store.counter + 1}
   }else if(action.type === 'DECREMENT') {
     store = {counter :  store.counter - 1}
+  }else if(action.type === 'ADDITION') {
+    store = {counter :  store.counter + Number(action.payload.number)}
+  }else if(action.type === 'SUBTRACTION') {
+    store = {counter :  store.counter - Number(action.payload.number)}
   }
   return store;
 }
 
-const store = createStore(reducer)
+const store = createStore(reducer, INITIAL_VALUE)
 
 export default store
